2ff4029466d010db7b58f6f9f21cce5938ebc82a
웅곤 김 <psopen11@naver.com>
2024-06-05 18:39:57 +0900

    Add project files.

---
+++ ColorCompare/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace ColorCompare
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnUploadImage1;
+        private System.Windows.Forms.Button btnUploadImage2;
+        private System.Windows.Forms.Button btnUploadImage3;
+        private System.Windows.Forms.Button btnDeleteImage1;
+        private System.Windows.Forms.Button btnDeleteImage2;
+        private System.Windows.Forms.Button btnDeleteImage3;
+        private System.Windows.Forms.Button btnErase;
+        private System.Windows.Forms.Label labelPixelInfo;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnUploadImage1 = new System.Windows.Forms.Button();
+            this.btnUploadImage2 = new System.Windows.Forms.Button();
+            this.btnUploadImage3 = new System.Windows.Forms.Button();
+            this.btnDeleteImage1 = new System.Windows.Forms.Button();
+            this.btnDeleteImage2 = new System.Windows.Forms.Button();
+            this.btnDeleteImage3 = new System.Windows.Forms.Button();
+            this.btnErase = new System.Windows.Forms.Button();
+            this.labelPixelInfo = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 260);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(260, 260);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(260, 260);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnUploadImage1
+            // 
+            this.btnUploadImage1.Location = new System.Drawing.Point(3, 3);
+            this.btnUploadImage1.Name = "btnUploadImage1";
+            this.btnUploadImage1.Size = new System.Drawing.Size(100, 30);
+            this.btnUploadImage1.TabIndex = 3;
+            this.btnUploadImage1.Text = "Upload Image 1";
+            this.btnUploadImage1.UseVisualStyleBackColor = true;
+            this.btnUploadImage1.Click += new System.EventHandler(this.btnUploadImage1_Click);
+            // 
+            // btnUploadImage2
+            // 
+            this.btnUploadImage2.Location = new System.Drawing.Point(109, 3);
+            this.btnUploadImage2.Name = "btnUploadImage2";
+            this.btnUploadImage2.Size = new System.Drawing.Size(100, 30);
+            this.btnUploadImage2.TabIndex = 4;
+            this.btnUploadImage2.Text = "Upload Image 2";
+            this.btnUploadImage2.UseVisualStyleBackColor = true;
+            this.btnUploadImage2.Click += new System.EventHandler(this.btnUploadImage2_Click);
+            // 
+            // btnUploadImage3
+            // 
+            this.btnUploadImage3.Location = new System.Drawing.Point(215, 3);
+            this.btnUploadImage3.Name = "btnUploadImage3";
+            this.btnUploadImage3.Size = new System.Drawing.Size(100, 30);
+            this.btnUploadImage3.TabIndex = 5;
+            this.btnUploadImage3.Text = "Upload Image 3";
+            this.btnUploadImage3.UseVisualStyleBackColor = true;
+            this.btnUploadImage3.Click += new System.EventHandler(this.btnUploadImage3_Click);
+            // 
+            // btnDeleteImage1
+            // 
+            this.btnDeleteImage1.Location = new System.Drawing.Point(109, 3);
+            this.btnDeleteImage1.Name = "btnDeleteImage1";
+            this.btnDeleteImage1.Size = new System.Drawing.Size(30, 30);
+            this.btnDeleteImage1.TabIndex = 6;
+            this.btnDeleteImage1.Text = "X";
+            this.btnDeleteImage1.UseVisualStyleBackColor = true;
+            this.btnDeleteImage1.Click += new System.EventHandler(this.btnDeleteImage1_Click);
+            // 
+            // btnDeleteImage2
+            // 
+            this.btnDeleteImage2.Location = new System.Drawing.Point(215, 3);
+            this.btnDeleteImage2.Name = "btnDeleteImage2";
+            this.btnDeleteImage2.Size = new System.Drawing.Size(30, 30);
+            this.btnDeleteImage2.TabIndex = 7;
+            this.btnDeleteImage2.Text = "X";
+            this.btnDeleteImage2.UseVisualStyleBackColor = true;
+            this.btnDeleteImage2.Click += new System.EventHandler(this.btnDeleteImage2_Click);
+            // 
+            // btnDeleteImage3
+            // 
+            this.btnDeleteImage3.Location = new System.Drawing.Point(321, 3);
+            this.btnDeleteImage3.Name = "btnDeleteImage3";
+            this.btnDeleteImage3.Size = new System.Drawing.Size(30, 30);
+            this.btnDeleteImage3.TabIndex = 8;
+            this.btnDeleteImage3.Text = "X";
+            this.btnDeleteImage3.UseVisualStyleBackColor = true;
+            this.btnDeleteImage3.Click += new System.EventHandler(this.btnDeleteImage3_Click);
+            // 
+            // btnErase
+            // 
+            this.btnErase.Location = new System.Drawing.Point(427, 3);
+            this.btnErase.Name = "btnErase";
+            this.btnErase.Size = new System.Drawing.Size(100, 30);
+            this.btnErase.TabIndex = 9;
+            this.btnErase.Text = "Erase";
+            this.btnErase.UseVisualStyleBackColor = true;
+            this.btnErase.Click += new System.EventHandler(this.btnErase_Click);
+            // 
+            // labelPixelInfo
+            // 
+            this.labelPixelInfo.AutoSize = true;
+            this.labelPixelInfo.Location = new System.Drawing.Point(533, 0);
+            this.labelPixelInfo.Name = "labelPixelInfo";
+            this.labelPixelInfo.Size = new System.Drawing.Size(72, 17);
+            this.labelPixelInfo.TabIndex = 10;
+            this.labelPixelInfo.Text = "Pixel Info: ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(260, 260);
+            this.panel1.TabIndex = 11;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(269, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(260, 260);
+            this.panel2.TabIndex = 12;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(535, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(262, 260);
+            this.panel3.TabIndex = 13;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.34F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 266);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.btnUploadImage1);
+            this.flowLayoutPanel1.Controls.Add(this.btnDeleteImage1);
+            this.flowLayoutPanel1.Controls.Add(this.btnUploadImage2);
+            this.flowLayoutPanel1.Controls.Add(this.btnDeleteImage2);
+            this.flowLayoutPanel1.Controls.Add(this.btnUploadImage3);
+            this.flowLayoutPanel1.Controls.Add(this.btnDeleteImage3);
+            this.flowLayoutPanel1.Controls.Add(this.btnErase);
+            this.flowLayoutPanel1.Controls.Add(this.labelPixelInfo);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 266);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 34);
+            this.flowLayoutPanel1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 300);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Image Pixel Info";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ ColorCompare/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+
+namespace ColorCompare
+{
+    public partial class Form1 : Form
+    {
+        private Mat img1, img2, img3;
+        private Mat img1Original, img2Original, img3Original;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Enable drag-and-drop for the picture boxes
+            pictureBox1.AllowDrop = true;
+            pictureBox2.AllowDrop = true;
+            pictureBox3.AllowDrop = true;
+
+            // Set up event handlers for drag-and-drop
+            pictureBox1.DragEnter += new DragEventHandler(PictureBox_DragEnter);
+            pictureBox2.DragEnter += new DragEventHandler(PictureBox_DragEnter);
+            pictureBox3.DragEnter += new DragEventHandler(PictureBox_DragEnter);
+
+            pictureBox1.DragDrop += new DragEventHandler(PictureBox_DragDrop);
+            pictureBox2.DragDrop += new DragEventHandler(PictureBox_DragDrop);
+            pictureBox3.DragDrop += new DragEventHandler(PictureBox_DragDrop);
+        }
+
+        private void btnUploadImage1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    img1 = Cv2.ImRead(openFileDialog.FileName);
+                    img1Original = img1.Clone();
+                    pictureBox1.Image = BitmapConverter.ToBitmap(img1);
+                }
+            }
+        }
+
+        private void btnUploadImage2_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    img2 = Cv2.ImRead(openFileDialog.FileName);
+                    img2Original = img2.Clone();
+                    pictureBox2.Image = BitmapConverter.ToBitmap(img2);
+                }
+            }
+        }
+
+        private void btnUploadImage3_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    img3 = Cv2.ImRead(openFileDialog.FileName);
+                    img3Original = img3.Clone();
+                    pictureBox3.Image = BitmapConverter.ToBitmap(img3);
+                }
+            }
+        }
+
+        private void PictureBox_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void PictureBox_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                if (files.Length > 0)
+                {
+                    string filePath = files[0];
+                    PictureBox pictureBox = sender as PictureBox;
+                    Mat img = Cv2.ImRead(filePath);
+
+                    if (pictureBox == pictureBox1)
+                    {
+                        img1 = img;
+                        img1Original = img.Clone();
+                        pictureBox1.Image = BitmapConverter.ToBitmap(img1);
+                    }
+                    else if (pictureBox == pictureBox2)
+                    {
+                        img2 = img;
+                        img2Original = img.Clone();
+                        pictureBox2.Image = BitmapConverter.ToBitmap(img2);
+                    }
+                    else if (pictureBox == pictureBox3)
+                    {
+                        img3 = img;
+                        img3Original = img.Clone();
+                        pictureBox3.Image = BitmapConverter.ToBitmap(img3);
+                    }
+                }
+            }
+        }
+
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (sender is PictureBox pictureBox && pictureBox.Image != null)
+            {
+                Mat img = pictureBox == pictureBox1 ? img1 : (pictureBox == pictureBox2 ? img2 : img3);
+                Mat imgOriginal = pictureBox == pictureBox1 ? img1Original : (pictureBox == pictureBox2 ? img2Original : img3Original);
+
+                if (img != null && imgOriginal != null)
+                {
+                    float imageAspectRatio = (float)img.Width / img.Height;
+                    float pictureBoxAspectRatio = (float)pictureBox.Width / pictureBox.Height;
+
+                    float scaleFactor;
+                    float offsetX = 0, offsetY = 0;
+
+                    if (imageAspectRatio > pictureBoxAspectRatio)
+                    {
+                        // Image is wider than PictureBox
+                        scaleFactor = (float)pictureBox.Width / img.Width;
+                        offsetY = (pictureBox.Height - img.Height * scaleFactor) / 2;
+                    }
+                    else
+                    {
+                        // Image is taller than PictureBox
+                        scaleFactor = (float)pictureBox.Height / img.Height;
+                        offsetX = (pictureBox.Width - img.Width * scaleFactor) / 2;
+                    }
+
+                    // Adjust coordinates
+                    int x = (int)((e.X - offsetX) / scaleFactor);
+                    int y = (int)((e.Y - offsetY) / scaleFactor);
+
+                    if (x < 0 || y < 0 || x >= img.Width || y >= img.Height)
+                        return;
+
+                    Vec3b pixel = img.At<Vec3b>(y, x);
+                    int r = pixel.Item2;
+                    int g = pixel.Item1;
+                    int b = pixel.Item0;
+
+                    // Convert RGB to HSV
+                    Mat pixelMat = new Mat(1, 1, MatType.CV_8UC3, new Scalar(b, g, r));
+                    Mat hsvMat = new Mat();
+                    Cv2.CvtColor(pixelMat, hsvMat, ColorConversionCodes.BGR2HSV);
+                    Vec3b hsvPixel = hsvMat.At<Vec3b>(0, 0);
+                    int h = hsvPixel.Item0;
+                    int s = hsvPixel.Item1;
+                    int v = hsvPixel.Item2;
+
+                    // Clone the original image to keep it intact
+                    Mat imgCopy = imgOriginal.Clone();
+
+                    // Draw a circle at the clicked point
+                    Cv2.Circle(imgCopy, new OpenCvSharp.Point(x, y), 5, new Scalar(255, 0, 0), -1);
+
+                    // Draw text on the image at the click position
+                    Cv2.PutText(imgCopy, $"RGB: ({r}, {g}, {b})", new OpenCvSharp.Point(x, y - 20), HersheyFonts.HersheySimplex, 0.5, Scalar.Green, 1);
+                    Cv2.PutText(imgCopy, $"HSV: ({h}, {s}, {v})", new OpenCvSharp.Point(x, y), HersheyFonts.HersheySimplex, 0.5, Scalar.Green, 1);
+                    Cv2.PutText(imgCopy, $"X: {x}, Y: {y}", new OpenCvSharp.Point(x, y + 20), HersheyFonts.HersheySimplex, 0.5, Scalar.Green, 1);
+
+                    pictureBox.Image = BitmapConverter.ToBitmap(imgCopy);
+
+                    // Save the updated image back to the original
+                    if (pictureBox == pictureBox1)
+                        img1 = imgCopy;
+                    else if (pictureBox == pictureBox2)
+                        img2 = imgCopy;
+                    else
+                        img3 = imgCopy;
+
+                    labelPixelInfo.Text = $"X: {x}, Y: {y}, RGB: ({r}, {g}, {b}), HSV: ({h}, {s}, {v})";
+                }
+            }
+        }
+
+        private void btnErase_Click(object sender, EventArgs e)
+        {
+            // Restore the original images
+            if (img1Original != null)
+            {
+                img1 = img1Original.Clone();
+                pictureBox1.Image = BitmapConverter.ToBitmap(img1);
+            }
+
+            if (img2Original != null)
+            {
+                img2 = img2Original.Clone();
+                pictureBox2.Image = BitmapConverter.ToBitmap(img2);
+            }
+
+            if (img3Original != null)
+            {
+                img3 = img3Original.Clone();
+                pictureBox3.Image = BitmapConverter.ToBitmap(img3);
+            }
+
+            labelPixelInfo.Text = "Pixel Info: ";
+        }
+
+        private void btnDeleteImage1_Click(object sender, EventArgs e)
+        {
+            img1 = null;
+            img1Original = null;
+            pictureBox1.Image = null;
+        }
+
+        private void btnDeleteImage2_Click(object sender, EventArgs e)
+        {
+            img2 = null;
+            img2Original = null;
+            pictureBox2.Image = null;
+        }
+
+        private void btnDeleteImage3_Click(object sender, EventArgs e)
+        {
+            img3 = null;
+            img3Original = null;
+            pictureBox3.Image = null;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.MouseClick += pictureBox_MouseClick;
+            pictureBox2.MouseClick += pictureBox_MouseClick;
+            pictureBox3.MouseClick += pictureBox_MouseClick;
+            btnErase.Click += btnErase_Click;
+            btnDeleteImage1.Click += btnDeleteImage1_Click;
+            btnDeleteImage2.Click += btnDeleteImage2_Click;
+            btnDeleteImage3.Click += btnDeleteImage3_Click;
+        }
+    }
+}
